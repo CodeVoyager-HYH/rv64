0b38178fb693410adcdd882d484ba349e79272c8
CodeVoyager-HYH <2084625064@qq.com>
2025-03-30 17:59:46 +0800

    error hello-str

---
+++ simulator/src/dpi/dpi.c
@@ -0,0 +1,64 @@
+#include <common.h>
+#include <cstdint>
+#include <cstdio>
+#include <defs.h>
+#include "verilated_dpi.h" // For VerilatedDpiOpenVar and other DPI related definitions
+
+
+extern "C" void dpi_ebreak(){
+	printf("下一个要执行的指令是ebreak\n");
+	SIMTRAP(0x80000000, 0);
+}
+
+extern "C" uint32_t dpi_instr_mem_read(uint64_t addr){
+  if(addr >= CONFIG_MBASE && addr < CONFIG_MBASE + CONFIG_MSIZE){
+		addr = addr & ~0x3u;
+		IFDEF(,Log("[fetch]addr = %lx,intsr = %lx",addr,pmem_read(addr, 4)));
+		return pmem_read(addr, 4);
+	}else{
+      printf("你将要访问的内存地址是0x%lx, 超出了内存区域\n", addr);
+      return 0;
+   }
+}
+
+extern "C" uint64_t dpi_mem_read(uint64_t addr, uint64_t len, uint32_t instr){
+	if(addr == 0) return 0;
+	else{
+		if(len == 2) addr = addr & ~0x1u;
+		else if(len == 4) addr = addr & ~0x3u;
+		else if(len == 8) addr = addr & ~0x7u;
+		uint64_t data = pmem_read(addr, len);
+
+		IFDEF(CONFIG_INS_TRACE,Log("[read instr = %x]addr = %lx,data = %lx,len = %ld",instr,addr,data,len));
+		return data;
+	}
+}
+
+extern "C" void dpi_mem_write(uint64_t addr, uint64_t data, int len, uint32_t instr, uint64_t pc){
+//	printf("store指令, 写入地址addr=[%lx], 写入数据wdata=[%lx], 写入长度len=[%d]\n", addr, data, len);
+	uint64_t error = 0x80000b30;
+	if(error!=NULL){
+		Log("0x80000b30 == %lx,intr = %x,addr = %lx,pc = %lx",pmem_read(0x80000b30, 4),instr,addr,pc);
+	}
+	if(addr == CONFIG_SERIAL_MMIO){
+		char ch = data;
+		printf("%c", ch);
+		fflush(stdout);
+	}else{
+		IFDEF(CONFIG_INS_TRACE,Log("[write instr = %x]addr = %lx,data = %lx,len = %d",instr,addr,data,len));
+		if(len == 2) addr = addr & ~0x1u;
+		else if(len == 4) addr = addr & ~0x3u;
+		else if(len == 8) addr = addr & ~0x7u;
+		
+		pmem_write(addr, len, data);
+	}
+	if(error!=NULL){
+		Log("0x80000b30 == %lx,intr = %x,addr = %lx, pc = %lx",pmem_read(0x80000b30, 4),instr,addr,pc);
+	}
+}
+
+
+extern uint32_t  *reg_ptr;
+extern "C" void dpi_read_regfile(const svOpenArrayHandle r) {
+  reg_ptr = (uint32_t *)(((VerilatedDpiOpenVar*)r)->datap());
+}
